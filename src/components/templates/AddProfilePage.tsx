e419576a530c983b53826a4fd18a857ead5c2851
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-10-08 08:37:41 +0330

    added submit handler for send data to server

--- src/components/templates/AddProfilePage.tsx
+++ src/components/templates/AddProfilePage.tsx
@@ -34,8 +34,18 @@ const AddProfilePage = () => {
     amenities: [],
   });
 
-  const submitHandler = () => {
-    console.log(profileData);
+  const submitHandler = async() => {
+   const res=await fetch("/api/profile",{
+    method:"POST",
+    body:JSON.stringify(profileData),
+    headers:{"Content-Type":"application/json"}
+   });
+   const data=await res.json();
+   if(data.error){
+    console.log("error");
+   }else {
+    console.log(data);
+   }
   };
 
   return (
