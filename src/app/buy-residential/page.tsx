816e1475c150678d255bdd8daca259e8a5bda846
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-10-16 17:42:24 +0330

    added style buyresidentialPage

--- src/app/buy-residential/page.tsx
+++ src/app/buy-residential/page.tsx
@@ -1,10 +1,14 @@
 import BuyResidentialPage from '@/templates/BuyResidentialPage';
 
+const BuyResidential = async () => {
+  const res = await fetch('http://localhost:3000/api/profile', {
+    cache: 'no-store',
+  });
+  const data = await res.json();
 
-const BuyResidential = () => {
-  return (
-    <BuyResidentialPage/>
-  )
-}
+  if (data.error) return <h3>مشکلی پیش آمده است.</h3>;
+
+  return <BuyResidentialPage data={data.data} />;
+};
 
 export default BuyResidential;
--- src/components/templates/BuyResidentialPage.tsx
+++ src/components/templates/BuyResidentialPage.tsx
@@ -1,9 +1,29 @@
+import Card from '@/module/Card';
+import { type Profile } from '@/templates/MyProfilePage';
 
-
-const BuyResidentialPage = () => {
-  return (
-    <div>BuyResidentialPage</div>
-  )
+interface BuyResidentialProps {
+  data: Profile[];
 }
 
-export default BuyResidentialPage
+const BuyResidentialPage = ({ data }: BuyResidentialProps) => {
+  return (
+    <div className="mt-20 flex justify-between">
+      <div className="shadow-form me-10 flex h-fit w-[220px] flex-col items-center rounded-xl px-[15px] py-[30px]">
+        sidebar
+      </div>
+      <div className="flex w-full flex-wrap justify-between">
+        {data.length ? null : (
+          <p className="bg-red-text h-[50px] rounded-xl px-[15] py-2.5 text-[1.3rem] text-red-500">
+            هیچ آگهی ثبت نشده است
+          </p>
+        )}
+
+        {data.map((profile) => (
+          <Card key={profile._id} {...profile} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BuyResidentialPage;
